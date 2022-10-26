2211419d8098ed3eb8c647f6a5f3e1352620649b
MateuszGawecki <72608321+MateuszGawecki@users.noreply.github.com>
2022-10-26 13:34:12 +0200

    author entity and dto tests

--- src/test/java/com/company/stories/model/dto/AuthorDTOTest.java
+++ src/test/java/com/company/stories/model/dto/AuthorDTOTest.java
@@ -1,4 +1,51 @@
 package com.company.stories.model.dto;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AuthorDTOTest {
+    private static final String authorName = "John";
+    private static final String authorSurname = "Smith";
+    private static final Long authorId = 1L;
+
+    @Test
+    void when_AuthorDTOBuilder_expect_AuthorDTO(){
+        //given
+
+        //when
+        AuthorDTO authorDTO = AuthorDTO.builder()
+                .authorId(authorId)
+                .authorName(authorName)
+                .authorSurname(authorSurname)
+                .build();
+
+        //then
+        assertEquals(authorId, authorDTO.getAuthorId());
+        assertEquals(authorName, authorDTO.getAuthorName());
+        assertEquals(authorSurname, authorDTO.getAuthorSurname());
+    }
+
+    @Test
+    void when_setAuthorNameAndSurname_expect_newNamesInEntity() {
+        //given
+        String newName = "Adam";
+        String newSurname = "Atkinson";
+
+        AuthorDTO authorDTO = AuthorDTO.builder()
+                .authorId(authorId)
+                .authorName(authorName)
+                .authorSurname(authorSurname)
+                .build();
+
+
+        //when
+        authorDTO.setAuthorName(newName);
+        authorDTO.setAuthorSurname(newSurname);
+
+        //then
+        assertEquals(authorId, authorDTO.getAuthorId());
+        assertEquals(newName, authorDTO.getAuthorName());
+        assertEquals(newSurname, authorDTO.getAuthorSurname());
+    }
 }
--- src/test/java/com/company/stories/model/entity/AuthorTest.java
+++ src/test/java/com/company/stories/model/entity/AuthorTest.java
@@ -1,4 +1,51 @@
 package com.company.stories.model.entity;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AuthorTest {
+    private static final String authorName = "John";
+    private static final String authorSurname = "Smith";
+    private static final Long authorId = 1L;
+
+    @Test
+    void when_AuthorBuilder_expect_Author(){
+        //given
+
+        //when
+        Author author = Author.builder()
+                .author_id(authorId)
+                .name(authorName)
+                .surname(authorSurname)
+                .build();
+
+        //then
+        assertEquals(authorId, author.getAuthor_id());
+        assertEquals(authorName, author.getName());
+        assertEquals(authorSurname, author.getSurname());
+    }
+
+    @Test
+    void when_setAuthorNameAndSurname_expect_newNamesInEntity() {
+        //given
+        String newName = "Adam";
+        String newSurname = "Atkinson";
+
+        Author author = Author.builder()
+                .author_id(authorId)
+                .name(authorName)
+                .surname(authorSurname)
+                .build();
+
+
+        //when
+        author.setName(newName);
+        author.setSurname(newSurname);
+
+        //then
+        assertEquals(authorId, author.getAuthor_id());
+        assertEquals(newName, author.getName());
+        assertEquals(newSurname, author.getSurname());
+    }
 }
