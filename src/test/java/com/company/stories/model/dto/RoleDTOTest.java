4bd847420c13cb63916436ab2424a00c0dc0444b
MateuszGawecki <72608321+MateuszGawecki@users.noreply.github.com>
2022-10-26 13:41:49 +0200

    UT role

--- src/test/java/com/company/stories/model/dto/RoleDTOTest.java
+++ src/test/java/com/company/stories/model/dto/RoleDTOTest.java
@@ -1,4 +1,44 @@
 package com.company.stories.model.dto;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RoleDTOTest {
+    private static final String roleName = "cleaner";
+    private static final Long roleId = 1L;
+
+    @Test
+    void when_RoleBuilder_expect_Role(){
+        //given
+
+        //when
+        RoleDTO role = RoleDTO.builder()
+                .roleId(roleId)
+                .name(roleName)
+                .build();
+
+        //then
+        assertEquals(roleId, role.getRoleId());
+        assertEquals(roleName, role.getName());
+    }
+
+    @Test
+    void when_setRole_expect_newRoleNameInEntity() {
+        //given
+        String newRoleName = "driver";
+
+        RoleDTO roleDTO = RoleDTO.builder()
+                .roleId(roleId)
+                .name(roleName)
+                .build();
+
+
+        //when
+        roleDTO.setName(newRoleName);
+
+        //then
+        assertEquals(roleId, roleDTO.getRoleId());
+        assertEquals(newRoleName, roleDTO.getName());
+    }
 }
--- src/test/java/com/company/stories/model/entity/RoleTest.java
+++ src/test/java/com/company/stories/model/entity/RoleTest.java
@@ -1,4 +1,44 @@
 package com.company.stories.model.entity;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RoleTest {
+    private static final String roleName = "cleaner";
+    private static final Long roleId = 1L;
+
+    @Test
+    void when_RoleBuilder_expect_Role(){
+        //given
+
+        //when
+        Role role = Role.builder()
+                .role_id(roleId)
+                .name(roleName)
+                .build();
+
+        //then
+        assertEquals(roleId, role.getRole_id());
+        assertEquals(roleName, role.getName());
+    }
+
+    @Test
+    void when_setRole_expect_newRoleNameInEntity() {
+        //given
+        String newRoleName = "driver";
+
+        Role role = Role.builder()
+                .role_id(roleId)
+                .name(roleName)
+                .build();
+
+
+        //when
+        role.setName(newRoleName);
+
+        //then
+        assertEquals(roleId, role.getRole_id());
+        assertEquals(newRoleName, role.getName());
+    }
 }
