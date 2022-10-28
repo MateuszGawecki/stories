9fe7fba1dbf1d28cfd487b83c83cd9617aed8853
MateuszGawecki <72608321+MateuszGawecki@users.noreply.github.com>
2022-10-28 15:08:58 +0200

    book test

--- src/test/java/com/company/stories/model/dto/BookDTOTest.java
+++ src/test/java/com/company/stories/model/dto/BookDTOTest.java
@@ -1,4 +1,72 @@
 package com.company.stories.model.dto;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BookDTOTest {
+    private static final Long bookId = 1L;
+    private static final String title = "Book of books";
+    private static final String description = "Lorem ipsum dolor";
+    private static final String image_path = "BoB1233211";
+    private static final Float global_score = 7.9F;
+    private static final Integer votes = 1000;
+    private static final Set<AuthorDTO> authors = new HashSet<>();
+    private static final Set<GenreDTO> genres = new HashSet<>();
+
+    @Test
+    void when_BookDTOBuilder_expect_BookDTO(){
+        //given
+
+        //when
+        BookDTO book = BookDTO.builder()
+                .bookId(bookId)
+                .title(title)
+                .description(description)
+                .imagePath(image_path)
+                .globalScore(global_score)
+                .votes(votes)
+                .authors(authors)
+                .genres(genres)
+                .build();
+
+        //then
+        assertEquals(bookId, book.getBookId());
+        assertEquals(title, book.getTitle());
+        assertEquals(description, book.getDescription());
+        assertEquals(image_path, book.getImagePath());
+        assertEquals(votes, book.getVotes());
+        assertEquals(global_score, book.getGlobalScore());
+    }
+
+    @Test
+    void when_setBookDTOTitleAndDesc_expect_newValuesInDTO() {
+        //given
+        String newTitle = "Book2";
+        String newDesc = "Lorem 2";
+
+        BookDTO book = BookDTO.builder()
+                .bookId(bookId)
+                .title(title)
+                .description(description)
+                .imagePath(image_path)
+                .globalScore(global_score)
+                .votes(votes)
+                .authors(authors)
+                .genres(genres)
+                .build();
+
+
+        //when
+        book.setTitle(newTitle);
+        book.setDescription(newDesc);
+
+        //then
+        assertEquals(bookId, book.getBookId());
+        assertEquals(newTitle, book.getTitle());
+        assertEquals(newDesc, book.getDescription());
+    }
 }
--- src/test/java/com/company/stories/model/entity/BookTest.java
+++ src/test/java/com/company/stories/model/entity/BookTest.java
@@ -1,4 +1,72 @@
 package com.company.stories.model.entity;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BookTest {
+    private static final Long bookId = 1L;
+    private static final String title = "Book of books";
+    private static final String description = "Lorem ipsum dolor";
+    private static final String image_path = "BoB1233211";
+    private static final Float global_score = 7.9F;
+    private static final Integer votes = 1000;
+    private static final Set<Author> authors = new HashSet<>();
+    private static final Set<Genre> genres = new HashSet<>();
+
+    @Test
+    void when_BookBuilder_expect_BookEntity(){
+        //given
+
+        //when
+        Book book = Book.builder()
+                .book_id(bookId)
+                .title(title)
+                .description(description)
+                .image_path(image_path)
+                .global_score(global_score)
+                .votes(votes)
+                .authors(authors)
+                .genres(genres)
+                .build();
+
+        //then
+        assertEquals(bookId, book.getBook_id());
+        assertEquals(title, book.getTitle());
+        assertEquals(description, book.getDescription());
+        assertEquals(image_path, book.getImage_path());
+        assertEquals(votes, book.getVotes());
+        assertEquals(global_score, book.getGlobal_score());
+    }
+
+    @Test
+    void when_setBookTitleAndDesc_expect_newValuesInEntity() {
+        //given
+        String newTitle = "Book2";
+        String newDesc = "Lorem 2";
+
+        Book book = Book.builder()
+                .book_id(bookId)
+                .title(title)
+                .description(description)
+                .image_path(image_path)
+                .global_score(global_score)
+                .votes(votes)
+                .authors(authors)
+                .genres(genres)
+                .build();
+
+
+        //when
+        book.setTitle(newTitle);
+        book.setDescription(newDesc);
+
+        //then
+        assertEquals(bookId, book.getBook_id());
+        assertEquals(newTitle, book.getTitle());
+        assertEquals(newDesc, book.getDescription());
+    }
 }
